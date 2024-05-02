40b9d43b67712e596d53ead48deb42f101cd2bb6
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-02 16:53:38 +0100

    List of reservations

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { ReservationServiceService } from '../reservation/reservation-service.service';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationServiceService) {}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations();
+  }
 
 }
